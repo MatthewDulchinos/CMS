95d72e4f3758a083de70c3e19308ef02a8c3350e
Matthew Dulchinos <mdulchinos@gmail.com>
2024-04-10 22:14:51 -0400

    Create basic logic for database creation and apis

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # CMS
+
+System requirements:
+Running on: Python 3.11.9 (Other versions of python 3.x probably work as well, but it was tested on 3.11.9)
+Required packages: flask, langchain_openai, openpyxl, sqlite3, argparse, os
+
+Background:
+This project is based off medical codes from the below link. These codes are used to be able tag key 
+https://www.cms.gov/medicare/regulations-guidance/physician-self-referral/list-cpt/hcpcs-codes
+
+Features:
+This project demonstrates a basic API that you can use to look up descriptions of codes, search for codes based off key phrases, and ask chatGPT what a code means. 
+
+In the future I would like to create a machine learning model, that predicts what codes people might search for, based off of codes they currently have, but that would require a dataset I do not currently have.
+
+
+Database:
+This program will create a database that contains all of the codes provided from https://www.cms.gov/apps/ama/license.asp?file=/files/zip/list-codes-effective-january-1-2024-published-november-29-2023.zip
+
+You can create a database using a different list code (i.e. a previous or future year) by downloading the file, renaming the excel file code_list.xlsx and placing it in the codes folder (replacing the current codes/code_list.xlsx file)
+
+APIs:
+
+GET /code/{id}:
+	path parameters:
+		id = the identify code
+	example: http://127.0.0.1:5000/code/Q9982
+	example response: 
+		{
+    		"code": "Q9982",
+    		"description": "Flutemetamol f18 diagnostic"
+		}
+	Functionality: fetches the description for a given code, returns 404 if the code isnt in the database
+
+GET /search:
+	Query parameters: 
+		prase = the keyword or keywords that you want to search for a code with
+	example: http://127.0.0.1:5000/search?phrase=colonography
+	example response: 
+		[
+		    {
+		        "code": "74261",
+		        "description": "Ct colonography dx"
+		    },
+		    {
+		        "code": "74262",
+		        "description": "Ct colonography dx w/dye"
+		    }
+		]
+	Functionality: searches the database for keyword(s) and returns any matches or a 404 if nothing is found
+
+GET /info/{id}:
+	path parameters:
+		id = the identify code
+	Query parameters: 
+		OPEN_API_KEY = an open api secret key (Get one from here assuming you have a chatGPT account: https://platform.openai.com/account/api-keys)
+	example: http://127.0.0.1:5000/info/Q9982?OPEN_API_KEY=ff-ffffffffffffffff
+	example response: 
+		content='The HCPCS code Q9982 and description "Flutemetamol f18 diagnostic" refer to a specific diagnostic radiopharmaceutical used in imaging studies, particularly in PET scans to detect beta-amyloid plaques in the brain. This procedure is commonly used in the diagnosis of Alzheimer\'s disease and other neurodegenerative disorders.'
+	Functionality: Asks chatGPT for a defintion of a particular code. This obviously is not perfect, works as an easy search to understand abreviations. (obviously passing a secret key over the internet is not ideal, but it works for local stuff)
---
+++ main.py
@@ -0,0 +1,122 @@
+#CMS demo:
+
+from flask import Flask, jsonify, request, g
+from langchain_openai import ChatOpenAI
+import openpyxl
+import sqlite3
+import argparse
+import os
+
+app = Flask(__name__)
+DATABASE = 'codes.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+@app.route('/code/<string:id>', methods=['GET'])
+def get_code(id):
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT code, description FROM codes WHERE code=?", (id,))
+    row = cursor.fetchone()
+    cursor.close()
+    if row:
+        code, description = row
+        return jsonify({'code': code, 'description': description})
+    else:
+        return jsonify({'error': 'Code not found'}), 404
+
+# API endpoint to search for a code based on a phrase
+@app.route('/search', methods=['GET'])
+def search_code():
+    search_phrase = request.args.get('phrase')
+    if not search_phrase:
+        return jsonify({'error': 'Search phrase is required'}), 400
+
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT code, description FROM codes WHERE description LIKE ?", ('%' + search_phrase + '%',))
+    rows = cursor.fetchall()
+    cursor.close()
+
+    if rows:
+        codes_list = [{'code': row[0], 'description': row[1]} for row in rows]
+        return jsonify(codes_list)
+    else:
+        return jsonify({'error': 'No codes found for the search phrase'}), 404
+
+@app.route('/info/<string:id>', methods=['GET'])
+def ask_chat_gpt(id):
+    llm = ChatOpenAI(openai_api_key=request.args.get('OPEN_API_KEY'))
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT code, description FROM codes WHERE code=?", (id,))
+    row = cursor.fetchone()
+    cursor.close()
+    if row:
+        code, description = row
+        try:
+            answer = llm.invoke(f"What does code: {code} and description: {description} mean from the HCPCS terminology?")
+            return str(answer)
+        except Exception as e:
+            return jsonify({'error': str(e)}), 401
+    else:
+        return jsonify({'error': 'Code not found'}), 404
+
+
+def create_codes_table(cursor):
+    cursor.execute('''CREATE TABLE IF NOT EXISTS codes
+                    (code TEXT,
+                     description TEXT,
+                     PRIMARY KEY (code))''')
+
+def create_database(filepath, cursor):
+    create_codes_table(cursor)
+    rows = extract_rows_from_excel('codes/code_list.xlsx')
+    for row in rows:
+        #print(row[0],row[1])
+        cursor.execute("INSERT INTO codes (code, description) VALUES (?, ?)", (row[0], row[1]))
+
+def extract_rows_from_excel(file_path):
+    # Load the Excel workbook
+    wb = openpyxl.load_workbook(file_path)
+    sheet = wb.active
+
+    #rows need to be a dictionary because there are duplicate records
+    rows = {}
+    for row in sheet.iter_rows(values_only=True):
+        #only columns where the row[1] is not empty are 
+        if row[1] is not None:
+            rows[str(row[0])] = row
+    return list(rows.values())
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Database recreation flag')
+    parser.add_argument('--recreateDb', action='store_true', help='Flag to recreate the database')
+    parser.add_argument('--debug', action='store_true', help='Flag to start flask in debug mode')
+    args = parser.parse_args()
+
+    if args.recreateDb:
+        print("Database requested to be recreated")
+        if os.path.exists(DATABASE):
+            os.remove(DATABASE)
+            print("Old database deleted")
+    if not os.path.exists(DATABASE):
+        print("Creating new database")
+        conn = sqlite3.connect(DATABASE)
+        cursor = conn.cursor()
+        
+        create_database('codes/code_list.xlsx',cursor)
+        conn.commit()
+        conn.close()
+        print("New database created")
+    app.run(debug=args.debug)
